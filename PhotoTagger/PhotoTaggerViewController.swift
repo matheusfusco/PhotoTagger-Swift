104c066b87c07b15174c343c7a560f82276a7690
Matheus Pacheco Fusco <matheus.fusco@finners.com.br>
2017-03-08 19:24:52 -0300

    Inserted Alamofire functions, upload is working properly. To do: Retrieve the data, and adjust tableview to receive it.

--- PhotoTagger/PhotoTaggerViewController.swift
+++ PhotoTagger/PhotoTaggerViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class PhotoTaggerViewController: UIViewController {
     
@@ -69,19 +70,95 @@ class PhotoTaggerViewController: UIViewController {
 
 }
 
+extension PhotoTaggerViewController {
+    func upload (image : UIImage,
+                 progressCompletion : @escaping ( _ percent : Float) -> Void,
+                 completion : @escaping ( _ tags : [String], _ colors : [PhotoColor]) -> Void){
+        guard let imageData = UIImageJPEGRepresentation(image, 0.5) else {
+            print("Could not get JPEG representation of image")
+            return
+        }
+        
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
+            multipartFormData.append(imageData,
+                                     withName: "imagefile",
+                                     fileName: "image.jpeg",
+                                     mimeType: "image/jpeg")
+        },
+        to: "http://api.imagga.com/v1/content",
+        headers : ["Authorization" : "Basic YWNjXzg0NTRjMGMyY2FiZDc0MzpiNTkyODU2MDllN2U0NzdiNjRlODQxZWViMzFmNjg2Yg=="],
+        encodingCompletion: { (encodingResult) in
+            switch encodingResult {
+                case .success(request: let upload, _, _) :
+                    upload.uploadProgress(closure: { (progress) in
+                        progressCompletion(Float(progress.fractionCompleted))
+                    })
+                    upload.validate()
+                    upload.responseJSON(completionHandler: { (response) in
+                        guard response.result.isSuccess else {
+                            completion([String](), [PhotoColor]())
+                            return
+                        }
+                        
+                        guard let responseJSON = response.result.value as? [String : Any], let uploadedFile = responseJSON["uploaded"] as? [[String : Any]], let firstFile = uploadedFile.first, let firstFileID = firstFile["id"] as? String else {
+                            print("Invalid information received from service")
+                            completion([String](), [PhotoColor]())
+                            return
+                        }
+                        
+                        print("File uploaded successfully: \(firstFileID)")
+                        
+                        completion([String](), [PhotoColor]())
+                    })
+                    break
+                
+                case .failure(let encodingError) :
+                    print(encodingError)
+                    break
+            }
+        })
+        
+    }
+}
 
+//MARK: - UIImagePickerController Delegate
 extension PhotoTaggerViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
         guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
             dismiss(animated: true, completion: nil)
             return
         }
         
         imageView.image = image
+        
+        takePhotoButton.isHidden = true
+        
+        progressView.progress = 0.0
+        progressView.isHidden = false
+        
+        activityIndicatorView.startAnimating()
+        
+        upload(image: image,
+               progressCompletion: { (percent) in
+                self.progressView.setProgress(percent, animated: true)
+        }) { (tags, colors) in
+            
+            self.takePhotoButton.isHidden = false
+            self.progressView.isHidden = true
+            self.activityIndicatorView.stopAnimating()
+            
+            self.tags = tags
+            self.colors = colors
+            
+            self.performSegue(withIdentifier: "ShowResults", sender: self)
+        }
+        
         dismiss(animated: true, completion: nil)
     }
 }
 
+//MARK: - UINavigationController Delegate
 extension PhotoTaggerViewController: UINavigationControllerDelegate {
     
 }
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PhotoTagger' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PhotoTagger
+	pod 'Alamofire'
+end
